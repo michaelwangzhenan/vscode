ee78128becada1fd175867871456614716b98bc8
Michael <81884775@qq.com>
2024-01-17 21:14:07 +0800

    day 10

--- 6.pandas/2.operation.py
+++ 6.pandas/2.operation.py
@@ -65,9 +65,62 @@ def index_func():
 
     print("f.loc['l2']")
     print(f.loc['l2'], '\n')  # loc 选的是行，对应废弃的ix 函数
+    print(f.loc[['l2', 'l1']], '\n')  # 选多个行，要传入的是list，也就这这里有两层[[]]
 
     print("f.loc['l2'][['c1', 'c3']]")
-    print(f.loc['l2'][['c1', 'c3']], '\n')
+    print(f.loc['l2'][['c1', 'c3']], '\n')  # 同时选取行和列
+
+    print("f > 5")
+    print(f > 5, '\n')
+
+    print("f[f > 5]")
+    print(f[f > 5], '\n')  # 按元素生成bool值
+
+    print("f['c1'] > 0")
+    print(f['c1'] > 0, '\n')
+
+    print("f[f['c1'] > 0]")
+    print(f[f['c1'] > 0], '\n')  # bool 切片返回的是行
 
 
-index_func()
+# index_func()
+
+
+def auto_align():
+    s1 = Series(np.arange(3), index=['a', 'b', 'c'])
+    s2 = Series(np.arange(3), index=['a', 'b', 'd'])
+    print("s1:")
+    print(s1, '\n')
+    print("s2:")
+    print(s2, '\n')
+    print("s1+s2:")
+    print(s1+s2, '\n')  # 相同index进行操作，不同的赋值 NaN
+
+    f1 = DataFrame(np.arange(9).reshape((3, 3)), index=['l1', 'l2', 'l3'],
+                   columns=['c1', 'c2', 'c3'])
+    f2 = DataFrame(np.arange(9).reshape((3, 3)), index=['l2', 'l3', 'l4'],
+                   columns=['c1', 'c2', 'c3'])
+    print("f1:")
+    print(f1, '\n')
+    print("f2:")
+    print(f2, '\n')
+    print("f1+f2:")
+    print(f1+f2, '\n')  # 相同二维 index [lx, cy]进行操作，不同的赋值 NaN
+
+    print("f1.add(f2, fill_value=0)")
+    print(f1.add(f2, fill_value=0), '\n')  # 使用 DataFrame 的 add 方法时可以指定缺失值， 注意: 指定的时缺失的那个，不是最终的结果
+
+    s3 = f1.loc['l1']
+    print("s3 = f1.ioc('l1')")
+    print(s3, '\n')
+    print("f1-s3")
+    print(f1-s3, '\n')  # DataFrame 和 Series 按行运算
+    print(f1.sub(s3), '\n')
+
+    s4 = f1['c2']
+    print("s4 = f1['c2']")
+    print(s4, '\n')
+    print(f1.sub(s4, axis=0))  # 按列计算只能用函数并指定轴，如add, sub, mul, div
+
+
+auto_align()
